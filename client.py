30a61ffb2b77a85a1c464b9c58eeaf9009b0ec28
nobody-codes <156836754+nobody-codes@users.noreply.github.com>
2024-08-10 17:22:19 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,52 @@
 import socket
 import sys
-client = socket.socket()
-host = '192.168.1.10'
-port = 9999
 
-client.connect((host, port))
+def create_socket():
+    try:
+        global host
+        global port
+        global client
+        
+        client = socket.socket()
+        host = '192.168.1.10'
+        port = 9999
+    except socket.error as msg:
+        print(f"Socket creation error: {str(msg)}")
+    
+def connect_socket():
+    try:
+        global host
+        global port
+        global client
+        
+        client.connect((host, port))
+        print(f"Connection has been established! | HOST {host}  | Port {port}")
+        start_chat(client)
+    except socket.error as msg:
+        print(f"Socket Connection error: {str(msg)}")
+    
+def start_chat(client):
+    try:
+        while True:
+            server_response = client.recv(1024).decode("utf-8")
+            if server_response == 'server: quit':
+                print("Sever: Ended Chat")
+                client.close()
+                break
+            else:
+                print(server_response)
+                send_msg = input("Type message: ")
+                client.send(str.encode("client: "+send_msg))
+                if send_msg == 'quit':
+                    print("client: Ended Chat")
+                    client.close()
+                    break
+    except socket.error as msg:
+        print(f"Chat Communication error : {str(msg)}")
 
-while True:
-    server_response = client.recv(1024).decode("utf-8")
-    if server_response == 'server: quit':
-        print("Sever: Ended Chat")
-        client.close()
-        break
-    else:
-        print(server_response)
-        send_msg = input("Type message: ")
-        client.send(str.encode("client: "+send_msg))
-        if send_msg == 'quit':
-            print("client: Ended Chat")
-            client.close()
-            break
-            
+
+def main():
+    create_socket()
+    connect_socket()
+    
+main()
